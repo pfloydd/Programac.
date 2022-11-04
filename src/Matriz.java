b2f06e5ffcb9f319ef24523fabcf7565b66cb5d9
pfloydd <gemaagemitaa@gmail.com>
2022-11-04 19:54:40 +0100

    .

--- src/Matriz.java
+++ src/Matriz.java
@@ -5,23 +5,27 @@ public class Matriz {
         final int columnas = 5;
         int[][] numero = new int[filas][columnas];
         RellenarMatriz(numero, filas, columnas);
-        MostraArray(numero, filas, columnas);
+        MostraArrayYSuma(numero, filas, columnas);
     }
 
     public static void RellenarMatriz(int[][] lista, int fil, int col) {
         for (int i = 0; i < fil; i++) {
             for (int j = 0; j < col; j++) {
                 lista[i][j] =(int)(Math.random()*10);
-                System.out.println("");
             }
         }
     }
 
-    public static void MostraArray(int[][] lista, int fil, int col) {
+    public static void MostraArrayYSuma(int[][] lista, int fil, int col) {
+        int suma=0;
         for (int i = 0; i < fil; i++) {
             for (int j = 0; j < col; j++) {
                 System.out.print(lista[i][j] + "\t");
+                suma+=lista[i][j];
             }
         }
+
+        System.out.println(suma);
     }
+
 }
